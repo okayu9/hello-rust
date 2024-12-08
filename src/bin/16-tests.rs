035f0efb46b3e6dcb88e3bc163cadac4a5fc93df
Yumeto Inaoka <hi@okayu.jp>
2024-12-08 20:31:51 +0900

    is_even関数を追加し、偶数判定のテストケースを実装

--- src/bin/16-tests.rs
+++ src/bin/16-tests.rs
@@ -2,9 +2,15 @@ fn add(a: i32, b: i32) -> i32 {
     a + b
 }
 
+fn is_even(num: i32) -> bool {
+    num % 2 == 0
+}
+
 fn main() {
     let sum = add(3, 5);
     println!("sum: {}", sum);
+
+    println!("Is 3 even? {}", is_even(3));
 }
 
 #[cfg(test)]
@@ -19,5 +25,15 @@ mod tests{
     #[test]
     fn test_add_negative_numbers(){
         assert_eq!(add(-5, -6), -11);
+    }
+
+    #[test]
+    fn test_is_even_true() {
+        assert!(is_even(2), "2 should be even");
+    }
+
+    #[test]
+    fn test_is_even_false() {
+        assert!(!is_even(5), "5 should not be even");
     }
 }
